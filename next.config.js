ea29844022520df05eb48dae8141e0502e6aca40
FlorianFe <webtutorialdesign@gmail.com>
2023-07-05 18:00:20 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/tax-defferal/out",
+  basePath: "/tax-defferal",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
